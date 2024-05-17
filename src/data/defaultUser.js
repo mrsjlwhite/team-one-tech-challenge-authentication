c78a1434a8061eaba0457adf0a754bdd902b885e
Jeslyn <jlwhite.codes@gmail.com>
2024-05-17 12:44:36 -0700

    Updated to handle default user

---
+++ src/data/defaultUser.js
@@ -0,0 +1,4 @@
+export const defaultUser = {
+    username: 'username',
+    password: 'password'
+}
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,15 +1,19 @@
+import { defaultUser } from "../data/defaultUser";
+
 let attempts = 0;
 let isLocked = false;
 
 const UserService = {
-    handleInvalidLogin: function (message) {
-        window.alert('Oops! A ' + message + ' is needed to login.');
+    handleInvalidLogin: function (msg, useCustomMessage = false) {
+        const message = useCustomMessage ? msg : 'Oops! A ' + msg + ' is needed to login.';
+        window.alert(message);
 
         if (attempts >= 2) {
             isLocked = true;
-            // todo lock form with settimeout
+            // todo lock form with setTimeout
         } else {
             attempts += 1;
+            isLocked = false;
         }
     },
     login: function (username, pass) {
@@ -26,6 +30,11 @@ const UserService = {
         if (!pass) {
             this.handleInvalidLogin('password');
             return;
+        }
+
+
+        if (defaultUser.username !== username && pass !== defaultUser.password) {
+            this.handleInvalidLogin('Uh no! Looks like the username & password do not match. Please re-enter and try again.', true);
         }
 
         window.alert('Successfully logged in. Welcome!');
