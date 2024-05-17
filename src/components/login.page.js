79eb5047f8cfc567f21506c2c5149186fd0a6f8a
Mitchell Klee <mklee@synergybis.com>
2024-05-17 15:49:57 -0400

    login html added

--- src/components/login.page.js
+++ src/components/login.page.js
@@ -1,8 +1,41 @@
+import React, { useState } from 'react';
+import './LoginForm.css';
 
 export function LoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Username:', username);
+    console.log('Password:', password);
+    
+  };
+
   return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <div>
+        <label>Login:</label>
+        </div>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
   );
-}
+};
